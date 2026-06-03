0a04e9006071c50e430b2278ce5d4bfd3c4d7d65
Isheeta <i.mishra@student.han.nl>
2026-06-04 01:37:06 +0200

    Update UPDATED_SYD.py

--- UPDATED_SYD.py
+++ UPDATED_SYD.py
@@ -2001,13 +2001,7 @@ if st.button(
     if "low-cost" in choice.lower():
         st.session_state.strategy_memory["supplier"] = "Low Cost Supplier"
     elif "reliable" in choice.lower():
-        st.session_state.strategy_memory["supplier"] = "Reliable Supplier"
-    else:
-        st.session_state.strategy_memory["supplier"] = "Split purchasing between both suppliers"
-
-    st.session_state.motivations[
-        f"Q{st.session_state.quarter}_Purchasing"
-    ] = motivation.strip()
+    
     if motivation.strip() == "":
             st.warning(
                 "Consider explaining your reasoning. Supply chain decisions should be justified using cost, service, risk or sustainability factors."
